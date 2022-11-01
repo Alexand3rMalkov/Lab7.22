25711d3ef9a675bc43424176ba4e038db9c8425a
Alexand3rMalkov <113231750+Alexand3rMalkov@users.noreply.github.com>
2022-11-01 09:56:28 +0300

    Update Lab7.22.c

--- Lab7.22/Lab7.22.c
+++ Lab7.22/Lab7.22.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_DEPRECATE
+/*#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
@@ -23,6 +23,41 @@ int main()
         break;
     case '/':
         printf("%f / %f = %.2f", x, y, (x/y));
+        break;
+            
+    default: printf("Команда не распознана");
+        break;
+    }
+
+}
+*/
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    float x, y;
+    char z;
+    setlocale(LC_ALL, "rus");
+    printf("Введите действие:\n");
+    scanf("%f %c %f", &x, &z, &y);
+    switch (z)
+    {
+    case '+':
+        printf("%.2f + %.2f = %.2f", x, y, (x+y));
+        break;
+    case '-':
+        printf("%.2f - %.2f = %.2f", x, y, (x-y));
+        break;
+    case '*':
+        printf("%.2f * %.2f = %.2f", x, y, (x*y));
+        break;
+    case '/':
+        printf("%.2f / %.2f = %.2f", x, y, (x/y));
+    case '^':
+        printf("%.2f^%.2f = %.2f", x, y, pow(x, y));
         break;
     default: printf("Команда не распознана");
         break;
